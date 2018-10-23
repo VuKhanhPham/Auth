a8a7025fde531739f12449ff633c0e72029e0567
ONevets <otraigusnevets@gmail.com>
2018-10-23 13:36:23 -0700

    testing

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ app.listen(PORT);
 //localhost:5000
 
 //testing branch
+/*
+0.1 Check remote url destination  git remote -v
+0.2 Check status                  git status
+1. Add All files.                 git add .
+2. Commit all files.              git commit -m "Message goes here"
+3. Push files to repository.      git push origin branch-name
+*/
